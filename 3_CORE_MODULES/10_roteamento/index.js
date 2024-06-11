438b017e656d65aa7f92b283b65d4700e9c28da5
Bruno Sgotti <brunojf@ciandt.com>
2024-06-11 17:11:37 -0300

    modulos 3_CORE_MODULES e 4_NPM_FUNDAMENTAL concluídos

---
+++ 3_CORE_MODULES/10_roteamento/index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+
+    const q = url.parse(req.url, true)
+    const filename = q.pathname.substring(1)
+
+    console.log(q.pathname, q.pathname.substring(2));
+
+    if (filename.includes('html')) {
+        if (fs.existsSync(filename)) {
+            fs.readFile(filename, function (err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write(data)
+                return res.end()
+            })
+        } else {
+            fs.readFile('404.html', function (err, data) {
+                res.writeHead(404, {'Content-Type': 'text/html'})
+                res.write(data)
+                return res.end()
+            })
+        }
+    }
+
+})
+
+server.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`);
+})
--- 3_CORE_MODULES/5_fs/index.js
+++ 3_CORE_MODULES/5_fs/index.js
@@ -1,11 +1,14 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = 3000
 
 const server = http.createServer((req, res) => {
-    
-    res.write('Oi HTTP')
-    res.end()
+    fs.readFile('mensagem.html', function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        return res.end()
+    })
 
 })
 
